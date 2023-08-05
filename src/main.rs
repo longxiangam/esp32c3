173cfbf15670df9aca7cc65308bf8721051c8d88
longxiangam <longxiangam@163.com>
2023-08-05 21:41:18 +0800

    显示屏驱动

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 #![no_main]
 
 
-mod lcd;
 
 use esp_backtrace as _;
 use esp_println::println;
@@ -12,8 +11,16 @@ use core::cell::RefCell;
 use core::fmt::{Debug, Display};
 use hal::riscv::_export::critical_section;
 use critical_section::Mutex;
+use embedded_graphics::image::{Image, ImageRaw, ImageRawLE};
+use embedded_graphics::mono_font::ascii::FONT_6X9;
+use embedded_graphics::mono_font::MonoTextStyle;
+use embedded_graphics::pixelcolor::{BinaryColor, Rgb565};
+use embedded_graphics::prelude::*;
+use embedded_graphics::primitives::{PrimitiveStyle, Triangle};
+use embedded_graphics::text::Text;
 
 use hal::gpio::{Gpio1, GpioPin, Unknown};
+use st7735_lcd::Orientation;
 
 static BUTTON: Mutex<RefCell<Option<Gpio0<Input<PullDown>>>>> = Mutex::new(RefCell::new(None));
 static BUTTON1: Mutex<RefCell<Option<Gpio1<Input<PullDown>>>>> = Mutex::new(RefCell::new(None));
@@ -47,7 +54,7 @@ fn main() -> ! {
     println!("Hello world!");
 
     let io = IO::new(peripherals.GPIO,peripherals.IO_MUX);
-    let mut led12 = io.pins.gpio12.into_push_pull_output();
+  /*  let mut led12 = io.pins.gpio12.into_push_pull_output();
     let mut led13 = io.pins.gpio13.into_push_pull_output();
     led12.set_low().unwrap();
     led13.set_low().unwrap();
@@ -68,7 +75,7 @@ fn main() -> ! {
 
      critical_section::with(|cs| BUTTON1.borrow_ref_mut(cs).replace(button1));
 
-     interrupt::enable(peripherals::Interrupt::GPIO, interrupt::Priority::Priority3).unwrap();
+     interrupt::enable(peripherals::Interrupt::GPIO, interrupt::Priority::Priority3).unwrap();*/
 
 /*
     let aaa: GpioPin<Unknown, Bank0G, IRA, PINTYPE, SIG, GPIONUM> = io.pins.gpio12;
@@ -78,8 +85,61 @@ fn main() -> ! {
      }
 
     let mut delay = Delay::new(&clocks);
+
+    let sclk = io.pins.gpio2;
+    let miso = io.pins.gpio0;
+    let mosi = io.pins.gpio3;
+    let cs = io.pins.gpio7;
+
+    let mut spi = hal::Spi::new(
+        peripherals.SPI2,
+        sclk,
+        mosi,
+        miso,
+        cs,
+        32u32.MHz(),
+        hal::spi::SpiMode::Mode0,
+        &mut system.peripheral_clock_control,
+        &clocks,
+    );
+    let rst =io.pins.gpio10.into_push_pull_output();
+    let dc = io.pins.gpio6.into_push_pull_output();
+
+    let rgb = true;
+    let inverted = false;
+    let width = 128;
+    let height = 160;
+
+    let mut display = st7735_lcd::ST7735::new(spi, dc, rst, rgb, inverted, width, height);
+
+    display.init(&mut delay).unwrap();
+    display.clear(Rgb565::BLACK).unwrap();
+    display
+        .set_orientation(&Orientation::Landscape)
+        .unwrap();
+    display.set_offset(0, 0);
+    let yoffset = 100;
+    let image_raw: ImageRawLE<Rgb565> =
+        ImageRaw::new(include_bytes!("../assets/ferris.raw"), 86);
+    let image = Image::new(&image_raw, Point::new(26, 8));
+    image.draw(&mut display).unwrap();
+    let thin_stroke = PrimitiveStyle::with_stroke(Rgb565::WHITE, 1);
+    Triangle::new(
+        Point::new(16, 16 + yoffset),
+        Point::new(16 + 16, 16 + yoffset),
+        Point::new(16 + 8, yoffset),
+    )
+        .into_styled(thin_stroke)
+        .draw(&mut display).expect("绘制失败");
+
+    let character_style = MonoTextStyle::new(&FONT_6X9, Rgb565::WHITE);
+
+    let text = Text::new("Hello e-g", Point::new(10, 11), character_style);
+    text.draw(&mut display).expect("");
+
+
     loop {
-        led2.toggle().unwrap();
+      /*  led2.toggle().unwrap();*/
         delay.delay_ms(500u32);
     }
 }
