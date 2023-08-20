cab750eab92831166e7e297ef796d6ab80eb7ef5
longxiangam <longxiangam@163.com>
2023-08-20 21:25:47 +0800

    墨水屏驱动

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,39 @@
 #![no_main]
 
 
+mod cn_font;
+pub(crate) mod epd_2in9wb;
 
 use esp_backtrace as _;
 use esp_println::println;
 use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, timer::TimerGroup, Rtc, IO, interrupt, gpio::{Event, Gpio0, Input, PullDown}, riscv, Delay, peripherals, esp_riscv_rt, TrapFrame};
 
 use core::cell::RefCell;
-use core::fmt::{Debug, Display};
+/*use core::fmt::{Debug, Display};*/
 use hal::riscv::_export::critical_section;
 use critical_section::Mutex;
 use embedded_graphics::image::{Image, ImageRaw, ImageRawLE};
 use embedded_graphics::mono_font::ascii::FONT_6X9;
-use embedded_graphics::mono_font::MonoTextStyle;
+use embedded_graphics::mono_font::{MonoFont, MonoTextStyle, MonoTextStyleBuilder};
 use embedded_graphics::pixelcolor::{BinaryColor, Rgb565};
 use embedded_graphics::prelude::*;
-use embedded_graphics::primitives::{PrimitiveStyle, Triangle};
-use embedded_graphics::text::Text;
+use embedded_graphics::primitives::{Line, PrimitiveStyle, Triangle};
+use embedded_graphics::text::{Baseline, Text, TextStyleBuilder};
 
 use hal::gpio::{Gpio1, GpioPin, Unknown};
-use st7735_lcd::Orientation;
+/*use st7735_lcd::Orientation;*/
+
+use epd_waveshare::{epd2in9::*, graphics::DisplayRotation, prelude::*};
+use embedded_graphics::{
+
+    pixelcolor::BinaryColor::On as Black,
+    pixelcolor::BinaryColor::{Off as White},
+
+};
+use epd_waveshare::prelude::Display;
 
 static BUTTON: Mutex<RefCell<Option<Gpio0<Input<PullDown>>>>> = Mutex::new(RefCell::new(None));
 static BUTTON1: Mutex<RefCell<Option<Gpio1<Input<PullDown>>>>> = Mutex::new(RefCell::new(None));
-
 
 
 #[entry]
@@ -52,7 +62,7 @@ fn main() -> ! {
     wdt0.disable();
     wdt1.disable();
     println!("Hello world!");
-
+    let mut delay = Delay::new(&clocks);
     let io = IO::new(peripherals.GPIO,peripherals.IO_MUX);
   /*  let mut led12 = io.pins.gpio12.into_push_pull_output();
     let mut led13 = io.pins.gpio13.into_push_pull_output();
@@ -83,106 +93,130 @@ fn main() -> ! {
 */     unsafe {
          riscv::interrupt::enable();
      }
-
-    let mut delay = Delay::new(&clocks);
-
-    let sclk = io.pins.gpio2;
-    let miso = io.pins.gpio0;
-    let mosi = io.pins.gpio3;
-    let cs = io.pins.gpio7;
-
-    let mut spi = hal::Spi::new(
-        peripherals.SPI2,
-        sclk,
-        mosi,
-        miso,
-        cs,
-        32u32.MHz(),
-        hal::spi::SpiMode::Mode0,
-        &mut system.peripheral_clock_control,
-        &clocks,
-    );
-    let rst =io.pins.gpio10.into_push_pull_output();
-    let dc = io.pins.gpio6.into_push_pull_output();
-
-    let rgb = true;
-    let inverted = false;
-    let width = 128;
-    let height = 160;
-
-    let mut display = st7735_lcd::ST7735::new(spi, dc, rst, rgb, inverted, width, height);
-
-    display.init(&mut delay).unwrap();
-    display.clear(Rgb565::BLACK).unwrap();
-    display
-        .set_orientation(&Orientation::Landscape)
-        .unwrap();
-    display.set_offset(0, 0);
-    let yoffset = 100;
-    let image_raw: ImageRawLE<Rgb565> =
-        ImageRaw::new(include_bytes!("../assets/ferris.raw"), 86);
-    let image = Image::new(&image_raw, Point::new(26, 8));
-    image.draw(&mut display).unwrap();
-    let thin_stroke = PrimitiveStyle::with_stroke(Rgb565::WHITE, 1);
-    Triangle::new(
-        Point::new(16, 16 + yoffset),
-        Point::new(16 + 16, 16 + yoffset),
-        Point::new(16 + 8, yoffset),
-    )
-        .into_styled(thin_stroke)
-        .draw(&mut display).expect("绘制失败");
-
-    let character_style = MonoTextStyle::new(&FONT_6X9, Rgb565::WHITE);
-
-    let text = Text::new("Hello e-g", Point::new(10, 11), character_style);
-    text.draw(&mut display).expect("");
+    /*
 
 
-    loop {
-      /*  led2.toggle().unwrap();*/
-        delay.delay_ms(500u32);
+        let sclk = io.pins.gpio2;
+        let miso = io.pins.gpio0;
+        let mosi = io.pins.gpio3;
+        let cs = io.pins.gpio7.into_ph_pull_output();
+
+        let mut spi = hal::Spi::new(
+            peripherals.SPI2,
+            sclk,
+            mosi,
+            miso,
+            cs,
+            32u32.MHz(),
+            hal::spi::SpiMode::Mode0,
+            &mut system.peripheral_clock_control,
+            &clocks,
+        );
+        let rst =io.pins.gpio10.into_push_pull_output();
+        let dc = io.pins.gpio6.into_push_pull_output();
+
+        let rgb = true;
+        let inverted = false;
+        let width = 128;
+        let height = 160;
+
+        let mut display = st7735_lcd::ST7735::new(spi, dc, rst, rgb, inverted, width, height);
+
+        display.init(&mut delay).unwrap();
+        display.clear(Rgb565::BLACK).unwrap();
+        display
+            .set_orientation(&Orientation::Landscape)
+            .unwrap();
+        display.set_offset(0, 0);
+        let yoffset = 100;
+        let image_raw: ImageRawLE<Rgb565> =
+            ImageRaw::new(include_bytes!("../assets/ferris.raw"), 86);
+        let image = Image::new(&image_raw, Point::new(26, 8));
+        image.draw(&mut display).unwrap();
+        let thin_stroke = PrimitiveStyle::with_stroke(Rgb565::WHITE, 1);
+        Triangle::new(
+            Point::new(16, 16 + yoffset),
+            Point::new(16 + 16, 16 + yoffset),
+            Point::new(16 + 8, yoffset),
+        )
+            .into_styled(thin_stroke)
+            .draw(&mut display).expect("绘制失败");
+
+        let character_style = MonoTextStyle::new(&FONT_6X9, Rgb565::WHITE);
+
+        let text = Text::new("Hello e-g", Point::new(10, 11), character_style);
+        text.draw(&mut display).expect("");*/
+
+        //墨水屏
+        let epd_sclk = io.pins.gpio2;
+        let epd_miso = io.pins.gpio0;
+        let epd_mosi = io.pins.gpio3;
+        let epd_cs = io.pins.gpio7.into_push_pull_output();
+        let epd_rst =io.pins.gpio10.into_push_pull_output();
+        let epd_dc = io.pins.gpio6.into_push_pull_output();
+        let mut spi = hal::Spi::new_no_cs(
+            peripherals.SPI2,
+            epd_sclk,
+            epd_mosi,
+            epd_miso,
+            32u32.MHz(),
+            hal::spi::SpiMode::Mode0,
+            &mut system.peripheral_clock_control,
+            &clocks,
+        );
+        let busy_in = io.pins.gpio11.into_pull_up_input();
+       /* let mut epd = Epd2in9::new(&mut spi, cs, busy_in, dc, rst, &mut delay).expect("");*/
+        let mut epd_device =epd_2in9wb::Epd2in9wb::new(&mut spi, epd_cs, busy_in, epd_dc, epd_rst,  &mut delay);
+
+        epd_device.unwrap().work();
+
+
+        loop {
+          /*  led2.toggle().unwrap();*/
+            delay.delay_ms(500u32);
+        }
     }
-}
-
-
-#[interrupt]
-fn GPIO(context: &mut esp_riscv_rt::TrapFrame) {
-    critical_section::with(|cs| {
-        println!("GPIO interrupt");
-        println!("{:?}",context);
 
 
 
-       let  button_is_high =  BUTTON
-            .borrow_ref_mut(cs)
-            .as_mut()
-            .unwrap()
-            .is_acore_interrupt_set();
+    #[interrupt]
+    fn GPIO(context: &mut esp_riscv_rt::TrapFrame) {
+        critical_section::with(|cs| {
+            println!("GPIO interrupt");
+            println!("{:?}",context);
 
-        let  button1_is_high =  BUTTON1
-            .borrow_ref_mut(cs)
-            .as_mut()
-            .unwrap()
-            .is_acore_interrupt_set();
 
-        if(button_is_high){
-            println!("按钮0 按下");
-        }
 
-        if(button1_is_high){
-            println!("按钮1 按下");
-        }
-        BUTTON
-            .borrow_ref_mut(cs)
-            .as_mut()
-            .unwrap()
-            .clear_interrupt();
+           let  button_is_high =  BUTTON
+                .borrow_ref_mut(cs)
+                .as_mut()
+                .unwrap()
+                .is_acore_interrupt_set();
 
-        BUTTON1
-            .borrow_ref_mut(cs)
-            .as_mut()
-            .unwrap()
-            .clear_interrupt();
+            let  button1_is_high =  BUTTON1
+                .borrow_ref_mut(cs)
+                .as_mut()
+                .unwrap()
+                .is_acore_interrupt_set();
 
-    });
-}
+            if(button_is_high){
+                println!("按钮0 按下");
+            }
+
+            if(button1_is_high){
+                println!("按钮1 按下");
+            }
+            BUTTON
+                .borrow_ref_mut(cs)
+                .as_mut()
+                .unwrap()
+                .clear_interrupt();
+
+            BUTTON1
+                .borrow_ref_mut(cs)
+                .as_mut()
+                .unwrap()
+                .clear_interrupt();
+
+        });
+    }
